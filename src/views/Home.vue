e9bc24be5fe1194ebadd63f893618ff7815dc40e
hvekriya <haresh.1994@hotmail.com>
2019-05-17 15:19:48 +0100

    sitemap

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,7 @@
         <p>
           <a href="/experience" class="btn btn-light m-2">Experience timeline</a>
           <a
-            href="/https://drive.google.com/file/d/1yqg2e0DIoe9NPoNETT-d9vmuRfEaGR5O/view?usp=sharing"
+            href="https://drive.google.com/file/d/1yqg2e0DIoe9NPoNETT-d9vmuRfEaGR5O/view?usp=sharing"
             class="btn btn-light m-2" target="_blank"
           >See my latest CV</a>
         </p>
