b1688c0e680b6a20260532f7bc66b15e5502c3d6
Haresh Vekriya <haresh.vekriya@gmail.com>
2020-05-26 00:12:00 +0100

    fixed issue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
         <div class="intro-text">
           <div class="intro-lead-in">Welcome to ReExperience!</div>
           <div class="intro-heading text-uppercase">We are very excited to work with you.</div>
-          <a class="btn btn-primary btn-xl text-uppercase" href="/contact">Get in touch</a>
+          <a class="btn btn-primary btn-xl text-uppercase" href="/contact-us">Get in touch</a>
         </div>
       </div>
     </header>
