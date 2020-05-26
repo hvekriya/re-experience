e016c47b3b78838906c6fae9ed815e9bf22d035f
Haresh Vekriya <haresh.vekriya@gmail.com>
2020-05-26 19:41:38 +0100

    Added in the form

--- src/views/ContactUs.vue
+++ src/views/ContactUs.vue
@@ -14,13 +14,14 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-12">
-            <form id="contactForm" name="sentMessage" novalidate="novalidate">
+            <form id="contactForm" role="form" method="POST" @submit.prevent="sendEmail">
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group">
                     <input
                       class="form-control"
                       id="name"
+                      v-model="name"
                       type="text"
                       placeholder="Your Name *"
                       required="required"
@@ -32,6 +33,7 @@
                     <input
                       class="form-control"
                       id="email"
+                      v-model="email"
                       type="email"
                       placeholder="Your Email *"
                       required="required"
@@ -43,6 +45,7 @@
                     <input
                       class="form-control"
                       id="phone"
+                      v-model="phone"
                       type="tel"
                       placeholder="Your Phone *"
                       required="required"
@@ -56,6 +59,7 @@
                     <textarea
                       class="form-control"
                       id="message"
+                      v-model="message"
                       placeholder="Your Message *"
                       required="required"
                       data-validation-required-message="Please enter a message."
@@ -63,17 +67,25 @@
                     <p class="help-block text-danger"></p>
                   </div>
                 </div>
-                <div class="clearfix"></div>
-                <div class="col-lg-12 text-center">
-                  <div id="success"></div>
+                <div class="col-md-6">
+                  <!--LOADING MESSAGE-->
+                  <div v-if="loadingTxt">
+                    <p class="mb-8 text-success">Delivering your email...</p>
+                  </div>
+                  <div v-if="success">
+                    <p
+                      class="mb-8 text-success"
+                    >Email sent! We will get back to you as soon as possible!</p>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <!--SUBMIT BUTTON-->
                   <button
-                    id="sendMessageButton"
-                    class="btn btn-primary btn-xl text-uppercase"
                     type="submit"
-                  >
-                    Send
-                    Message
-                  </button>
+                    class="btn btn-primary"
+                    style="width:100%;"
+                    :disabled="!name || !email || !message"
+                  >Submit</button>
                 </div>
               </div>
             </form>
@@ -86,13 +98,50 @@
 
 <script>
 import ContactDetails from "../components/ContactDetails";
+import axios from "axios";
+
 export default {
   name: "ContactUs",
   data() {
-    return {};
+    return {
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+      loadingTxt: false,
+      success: false
+    };
   },
   components: {
     ContactDetails
+  },
+  methods: {
+    sendEmail() {
+      this.loadingTxt = true;
+      axios
+        .post("https://formspree.io/mdowezpe", {
+          name: this.name,
+          from: this.email,
+          _subject: `${this.name} | Via Re-Experience`,
+          phone: this.phone,
+          message: this.message
+        })
+        .then(response => {
+          this.name = "";
+          this.email = "";
+          this.phone = "";
+          this.message = "";
+          this.loadingTxt = false;
+          //i redirect my app to '/success' route once payload completed.
+          this.$router.push({ path: "/contact-us" });
+          this.success = true;
+        })
+        .catch(error => {
+          if (error.response) {
+            alert(error.response.data); // => the response payload
+          }
+        });
+    }
   }
 };
 </script>
