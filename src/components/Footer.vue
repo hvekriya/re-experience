e28d2ad2c9bb4005cbbad5f21a08973c1734fe49
Haresh Vekriya <haresh.vekriya@gmail.com>
2020-05-26 10:54:10 +0100

    Removed placeholders

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,26 +6,26 @@
         <div class="col-md-4">
           <span class="copyright">Copyright &copy; ReExperience 2020</span>
         </div>
-        <div class="col-md-4">
+        <!-- <div class="col-md-4">
           <ul class="list-inline social-buttons">
             <li class="list-inline-item">
-              <a href="https://www.facebook.com/ReExperienceConsulting/">
+              <a href="https://www.facebook.com/ReExperienceLtd/">
                 <i class="fab fa-twitter"></i>
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="https://www.facebook.com/ReExperienceConsulting/">
+              <a href="https://www.facebook.com/ReExperienceLtd/">
                 <i class="fab fa-facebook-f"></i>
               </a>
             </li>
             <li class="list-inline-item">
-              <a href="https://www.facebook.com/ReExperienceConsulting/">
+              <a href="https://www.facebook.com/ReExperienceLtd/">
                 <i class="fab fa-linkedin-in"></i>
               </a>
             </li>
           </ul>
-        </div>
-        <div class="col-md-4">
+        </div>-->
+        <!-- <div class="col-md-4">
           <ul class="list-inline quicklinks">
             <li class="list-inline-item">
               <a href="/about-us">Privacy Policy</a>
@@ -34,7 +34,7 @@
               <a href="/about-us">Terms of Use</a>
             </li>
           </ul>
-        </div>
+        </div>-->
       </div>
     </div>
   </footer>
